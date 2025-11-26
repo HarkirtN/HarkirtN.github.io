eb60a2c79b0f74832fca4d65a10f70bc21098c96
HarkirtN <harkirts@gmail.com>
2025-11-26 20:58:57 +1300

    starting javascript

---
+++ experiment.js
@@ -0,0 +1,15 @@
+const timesToRepeat = 10
+const character = 'loopy'
+
+//some loop that take the character and repeats it many times//
+let loopy = 0;
+while (loopy < 10) {
+    console.log ('loopy');
+}
+
+for (let i = 0; i < timesToRepeat; i++) {
+    answer = answer + character;
+}
+
+console.log(loopy)
+// expected result is repeating 'loop' 10x //
