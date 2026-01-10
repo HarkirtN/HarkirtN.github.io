8c01e9bec61c5b6603d93bf3b68e6740901906a3
HarkirtN <harkirts@gmail.com>
2026-01-10 21:56:41 +1300

    event listener notes written

--- events.js
+++ events.js
@@ -19,3 +19,44 @@ input.addEventListener("keyup", function () {
     paragraph.innerText = input.value; 
 });
 
+.color-box {
+   background-color : limegreen
+   width : 100px 
+   height : 100px
+}
+<div class = "color-box"></div>
+<input class= "color-input" placeholder = "type color here!"></input>
+
+const input1 = document.querySelector(".color-input") ;
+const paragraph1 = document.querySelector(".color-box"); 
+
+input.addEventListener("change", function () {
+    paragraph.style.backgroundColor = input.value ;
+});
+
+//changes the colour of div box as the color chosen in input bar
+// the change event will only happen if something is changed in the input bar and 
+// you've clicked somewhere i.e. changed whats written in input bar
+
+//event delegation is when you want to have something occur to all events 
+//example is alert of a div of buttons 
+<div class= "button-container">
+    <button> 1 </button>
+    <button> 2 </button>
+    <button> 3 </button>
+    <button> 4 </button>
+    <button> 5 </button>
+</div>
+
+document.querySelector(".button-container");
+.addEventListener ("click", function (event) {
+    alert('you clicked on button $[event.target.innerText]');
+});
+
+//create a function that when you click on any button under the parent 
+//you will get that message 
+// the gotcha is innertext will mean if you click inbetween the button
+// you will also receive the alert - to mitigate this you would place 
+// a cond clause (if target = button then carry out function (event)) 
+// else return console.log "no button clicked"
+
