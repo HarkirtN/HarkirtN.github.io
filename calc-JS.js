8de4a6f8d06335e425702f68d11979d4390cce4d
HarkirtN <harkirts@gmail.com>
2026-01-23 20:05:46 +1300

    going through expert example

---
+++ calc-JS.js
@@ -0,0 +1,83 @@
+//first attempt
+/*function add(number) {
+    return number + number;
+}
+
+function subtract(number) {
+    return  number - number
+}
+
+function multiply(number) {
+    return number * number
+}
+
+function divide(number) {
+    return number / number
+}
+
+function backspace (finalAnswer) { 
+    return finalAnswer.pop
+}
+
+let finalAnswer = function () {
+}*/
+
+//working through it with joseph
+let buffer = [] // an area where you want this to change with every input
+
+function bufferInput(value) {
+    buffer.push(value) //pushing on a number so if you add 6 the click 5 = 56
+    document.getElementById('input').value = formatBuffer() //connects the input bar at top of calc 
+}
+
+function compute() {
+    const value = eval(formatBuffer()); //shortcut to evaluating numbers that were originally strings 
+    buffer = []
+    bufferInput(value) // lets the value show on the buffer
+}
+
+function formatBuffer() {
+    return buffer.join(''); //joins the string together before it get eval i.e. 5 + 6 turns to '5+6'
+}
+
+//brian's way 
+
+let buffer = '0';
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+//console.log(value);
+if (isNaN(parseInt(value))) //converts the value from string to number or NaN
+    {handleSymbol(value); 
+}
+else  
+    {handleNumber(value);
+    }
+rerender(); //now that we can connect it to screen 
+}
+
+function handleNumber(number) {
+//console.log('number')
+if (buffer === '0') {
+    buffer = number; //this will keep it to 0 if you press zero and not 0000
+} else {buffer += number; //adding a number to the end
+}
+//console.log(buffer)
+}
+
+function handleSymbol(symbol) {
+//console.log('symbol')
+}
+
+function initial {
+    document.querySelector('calc-buttons') //selected the buttons
+    .addEventListener("click", function(event) { //everytime you get a click
+        buttonClick(event.target.innerText); // do buttonclick which targets the innertext of the button/div
+    });
+}
+ init (); // show it outside of scope 
+
+ function rerender () {
+    screen.innerText = buffer //it will make buffer = screen everytime i rerender
+ }
