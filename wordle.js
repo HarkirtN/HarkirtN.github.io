34f9032a349a9d21a9ae94a37bf29dea587d5299
HarkirtN <harkirts@gmail.com>
2026-02-09 13:03:58 +1300

    added an invalid function and debugged wordle

--- wordle.js
+++ wordle.js
@@ -68,133 +68,135 @@ function compareLetters(A, B) {
 //brian's way //
 const letters = document.querySelectorAll('.letter');
 const loadingDiv = document.querySelector('.info-bar');
-const ANSWER_LENGTH = 5; // is creaming case because its constant
+const ANSWER_LENGTH = 5;
 const ROUNDS = 6; // used later to validate whether win or lose - stop from extra keypresses 
 
+//do a async function can do await whenever you want
 async function init() {
- let currentGuess = '';
- let currentRow = 0;
- let isLoading = true;
+    let currentGuess = '';
+    let currentRow = 0;
+    let isLoading = true;
 
-const res = await fetch("https://words.dev-apis.com/word-of-the-day");
-const resObj = await res.json(); //similar to stringify
-const word = resObj.word.toUpperCase(); // make sure everything is in uppercase for consistancy
-const wordparts = word.split(""); // split the word into single letters - thats what split does
-let done = false; //initialise done variable 
-setLoading(false); 
-isLoading = false;
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day"); //res is short for response
+    const resObj = await res.json(); //similar to stringify
+    const word = resObj.word.toUpperCase(); // make sure everything is in uppercase for consistancy
+    const wordParts = word.split(""); // split the word into single letters - thats what split does
+    let done = false;
+    setLoading(false);
+    isLoading = false;
 
-    function addLetter(letter) {
-        if (currentGuess.length < ANSWER_LENGTH) {
-            currentGuess += letter; //adding on the letter to the current guess i.e. f=r=i
-        } else {
-            //replaces last letter
-            currentGuess = currentGuess.substring(0, currentGuess.length-1) + letter; 
-            //lops of the ketter at 5 and replaces with new letter
-            // say f g h j k and you type r it will replace k with r
-        }
+    console.log(word);
 
-        letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
-        //keep track of the letter 
-        // because its starts at 0, you need to -1 so when you ask for charAt it will know 
-        // answer length is 5 and multiply by rows will letter the DOM know what space it need tp write next letter
-    };
+function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+        currentGuess += letter; //adding on the letter to the current guess i.e. f=r=i
+    } else {
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
+        //lops off the letter at 5 and replaces with new letter
+        // say f g h j k and you type r it will replace k with r
+    }
+ // rerender bit 
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter; // link letter to letters at the end
+}
 
-    async function commit() {
-        if (currentGuess.length != ANSWER_LENGTH) {
-            //do nothing 
-            return;
-        }
-         
-        // validate the word 
-        isLoading = true; //we are loading something from an API
-        setLoading(true);
-        const res = await fetch("https://words.dev-apis.com/validate-word", {
-            method: "POST", 
-            body: JSON.stringify({ word: currentGuess})
-        });
-
-        const resObj = await res.json();
-        const validWord = resObj.validWord;
-        // const { validWOrd } = resObj
-
-        isLoading = false;
-        setLoading = false;
-
-        if (!validWord) {
-            markInvalidWord();
-            return; // return needed so it doesnt move forward to guess onto next line
-        }
-        //all marking is correct, close, wrong 
-        const guessParts = currentGuess.split("");
-        const map = makeMap(wordparts); // count occurence in fetched word 
-        console.log(map);
-
-         for (let i = 0; i < ANSWER_LENGTH; i++) {
-            //declare it being correct
-            if (guessParts[i] === wordparts[i]) {
-                letters[currentRow * ANSWER_LENGTH + i].classList.add("correct"); 
-                //talk to DOM saying letters is currentrow, 5 letters and add i - add the class list correct
-                map[guessParts[i]]--; // says to recurse and n - 1 the return occurence
-            }
-         }
-
-         for (let i = 0 ; i < ANSWER_LENGTH; i++) {
-            if (guessParts[i] === wordparts[i]) {
-                //do nothing, already done 
-            } else if (wordparts[i].includes(guessParts[i]) && map[guessParts[i]] > 0) {
-                letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
-                map[guessParts[i]]--; 
-            } else {
-                letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong")
-            }
-         }
-         //once commited then the buffers initialise 
-        currentRow++;
-        
-        if (currentGuess === word) {
-            //win
-            alert ('you win!');
-            done = true;
-            document.querySelector('.name').classList.add(".winner");
-         } else if (currentRow === ROUNDS) {
-            alert('you lose, the word id ${word}');
-            done = true;
-         }
-       currentGuess = ''; // move guess at bottom because if it above the word then you passing an empty string which 
-       // will never be correct === word 
+async function commit() {
+    if (currentGuess.length != ANSWER_LENGTH) {
+        return;
     }
     
+    //validate word
+    isLoading = true; //we are loading something from an API
+    setLoading(true);
+    //post API to ask if it is a valid 5 letter word 
+    const res = await fetch("https://words.dev-apis.com/validate-word", {
+        method: "POST",
+        body: JSON.stringify({ word: currentGuess })
+    });
 
-    function backspace() {
-        currentGuess = currentGuess.substring(0, currentGuess-1); //take length minus 1
-        letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = '';
-        // you want to remove '-1' for the DOM and replace with empty string so then i you
-        //backspace again it will move to the next letter back and remove one 
+    const resObj = await res.json();
+    const validWord = resObj.validWord;
+    // const { validWOrd } = resObj
+
+    isLoading = false;
+    setLoading(false);
+    
+    if (!validWord) { // we want this up here so it doesnt move through the word to guessParts
+        //alert('Not valid!')
+        markInvalidWord();
+        return; // return needed so it doesnt move forward to guess onto next line
     }
 
-    function markInvalidWord {
-        alert('Not a valid word!');
+    //marking is correct, close, wrong
+    const guessParts = currentGuess.split("");
+    const map = makeMap(wordParts); // count occurence in fetched word 
+    console.log(map);
 
-       for (let i = 0; i < ANSWER_LENGTH; i++) {
+    //go through correct function first to mark it up, then remove letter from guess tally
+    // to commpare again 
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+        if (guessParts[i] === wordParts[i]) {
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("correct"); // index into the letter and mark it correct
+            map[guessParts[i]]--; // now removing the letter from tally
+        }
+    }
+    
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+        if (guessParts[i] === wordParts[i]) {
+            //nothing
+        } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) { // now you can mark && to 
+            //say if there is letters more than 0 after removing from tally then its close  - recurse 
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+            map[guessParts[i]]--;
+        } else {
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong"); 
+        }      
+    }
+
+     //once commited then the buffers initialise 
+    currentRow++;
+    currentGuess = '';
+    // did they win - want it down here so it turns up green 
+    if (currentGuess === word) {
+        alert('You Win YAY!!');
+        done = true;
+        document.querySelector('.name').classList.add(".winner");
+        return; // to help terminate 
+    }
+    //did they lose
+    else if (currentRow === ROUNDS) {
+        alert('you lose, word was ${word}');
+        done = true;
+    }
+     // then wanna go to next row 
+     // if it goes higher than you are giving an empty string for currentGuess
+ }
+
+ function backspace() {
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1); //take length minus 1
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";  // replace last letter with empty string 
+   }
+
+   function markInvalidWord() {
+    alert('NOT VALID!');
+    // can add animation too 
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
         letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
 
-        setTimeout(function () { 
-           letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid") 
-        }, 10); // time in milisecs in the end 
-       } //you can't remove anything that isnt 'added' so last bit should be .add
-       // when it recurses then you can remove then add again
+        setTimeout(function() {
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid");
+        }, 10); // so as we do keypress it will remove the invalid then add it again if its invalid
     }
-    
-    document.addEventListener('keydown', function handleKeyPress(event) {
+   }
+
+    document.addEventListener('keydown', function keyPress(event) { //name is optional but helps when you're debugging
         if (done || isLoading) {
-            //do nothing 
             return;
         }
-
+        
         const action = event.key;
        // console.log(action);
 
+        //keep event listener simple 
         if (action === 'Enter') {
             commit();
         } else if (action === 'Backspace') {
@@ -202,32 +204,42 @@ isLoading = false;
         } else if (isLetter(action)) {
             addLetter(action.toUpperCase()) //converted to uppercase to allow consistency
         } else {
-            //do nothing
+            //do nothing 
         }
     });
 }
-init();
 
 function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter); //give it parameters 
+    return /^[a-zA-Z]$/.test(letter);
 }
 
-function setLoading() {
+// toggle will change between what you set, 
+// in this case to hidden or visible depending when true or false 
+function setLoading(isLoading) {
     loadingDiv.classList.toggle('show', isLoading);
-    //this is a boolean where you grab the class 'hidden' and applies if true 
-    //toggle adds or removes a CSS class element - so i would add show when i want to 
-    //show the loading sign, once i get my word i will change loading Div to false
 }
 
-function makeMap (Array) { // this function counts the occurence of letter
-    const obj = (i);
-    for (let i = 0; i < Array.length; i++) { // just means recurse 
-        const letter = Array[i] // to simplify rather than writing array[i]
-        if (obj[letter]) {
+function makeMap(array) { // this function counts the occurence of letter
+    const obj = {}; // make an onject i.e. grouping of data 1 x "P", 1 x "Y"
+    for (let i = 0; i < array.length; i++) {
+        const letter = array[i] // just to simplfy when writing it out in next lines
+        if (obj[letter]) { // is there a [letter]
             obj[letter]++; //count how many array[i] occur using recurse 
         } else {
-            obj[letter] = 1; 
+            obj[letter] = 1;
         }
-        return obj;
     }
+    return obj;
 }
+ //call the async function
+init();
+
+
+
+
+
+
+
+
+
+  
